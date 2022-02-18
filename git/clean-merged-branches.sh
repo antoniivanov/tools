e67c02df6d517439977602e7e9631db6715de594
Antoni Ivanov <aivanov@vmware.com>
2022-02-18 17:36:39 +0200

    add clean-merged-branches.sh

---
+++ git/clean-merged-branches.sh
@@ -0,0 +1,35 @@
+git checkout main
+git up origin main
+
+branches=$(git branch -l | grep -v 'main')
+
+upto_branches=()
+failed_branches=()
+
+for branch in $branches; do
+    if [ "$branch" != 'main' -a "$branch" != 'person/aivanov/vdk-core' -a "$branch" != 'person/aivanov/vdk-core-config' ];
+    then
+        echo ""
+        echo "$branch"
+        git checkout "$branch"
+        if git up origin main ; then
+            # git ls -n 3 | head
+            if git branch --contains HEAD | grep main; then
+                echo "Branch $branch up-to date with origin/main"
+                upto_branches+=("$branch")
+            else
+                echo "Branch $branch has missing commits in origin/main"
+            fi
+        else
+            git rebase --abort
+            failed_branches+=("$branch")
+        fi
+    fi
+done
+
+echo "Failed to rebase branches:"
+echo "${failed_branches[@]}"
+echo ""
+echo ""
+echo "Ready branches:"
+echo "${upto_branches[@]}"
